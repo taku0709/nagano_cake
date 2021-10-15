f2a7294b4035832aaef3100b580271fde032d653
EC2 Default User <ec2-user@ip-172-31-77-91.ec2.internal>
2021-10-15 14:22:09 +0000

    [add]devise:views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_contoroller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postal_code, :address, :telephone_number])
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  before_action :configure_permitted_parameters, if: :devise_contoroller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postal_code, :address, :telephone_number])
-  end
+ 
 end
