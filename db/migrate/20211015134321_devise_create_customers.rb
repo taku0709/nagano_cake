4cdde794ffaa8c3a0aa5872c7a269b42dc79cf95
EC2 Default User <ec2-user@ip-172-31-77-91.ec2.internal>
2021-10-15 14:10:16 +0000

    [add]customersにカラムを追加

--- db/migrate/20211015134321_devise_create_customers.rb
+++ db/migrate/20211015134321_devise_create_customers.rb
@@ -31,6 +31,18 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :encrypted_password
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.boolean :is_active
+      t.datetime :created_at
+      t.datetime :updated_at
+
 
 
       t.timestamps null: false
