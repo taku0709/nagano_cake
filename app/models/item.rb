29e326fd925d3c942a8cc6370436c13d549175a2
taku0709 <ai3.ca7@gmail.com>
2021-10-30 05:06:18 +0000

    [add]商品詳細画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
-  belongs_to :genres
+  belongs_to :genre
   attachment :image
   
-  enum is_active: {on_sale: 0, off_sale: 1}
+  # enum is_active: {on_sale: 0, off_sale: 1}
 end
