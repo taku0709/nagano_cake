2fa410fd11191e8af71cbd6145e12c681cb206d1
taku0709 <ai3.ca7@gmail.com>
2021-11-09 13:49:07 +0000

    [add]配送先編集画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,37 @@
 class Public::AddressesController < ApplicationController
+
+ def index
+   @addresses = Addresse.all
+   @address = Address.new
+ end
+
+ def create
+   @address = Address.new(address_params)
+   @address.save
+   redirect_to public_addressese_path
+ end
+
+
+ def edit
+   @address = Address.find(params[:id])
+ end
+
+ def update
+   @address = Address.find(params[:id])
+   @address.update
+   redirect_to public_addressese_path
+ end
+
+ def destroy
+   @address = Address.find(params[:id])
+   @address.destroy
+   redirect_to public_addressese_path
+ end
+
+ private
+
+ def address_params
+   params.require(:address).permit(:name, :address, :postal_code)
+ end
+
 end
