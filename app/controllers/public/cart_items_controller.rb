795bc6cc085608f20e3397a8de06f81afc6b993f
taku0709 <ai3.ca7@gmail.com>
2021-11-11 11:54:25 +0000

    [add]商品詳細画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,12 @@
 class Public::CartItemsController < ApplicationController
+
+
+
+
+
+  private
+  
+   def cart_item_params
+     params.require(:cart_item).permit(:item_id, :amount)
+   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,4 +2,15 @@ class Public::ItemsController < ApplicationController
   def index
    @items = Item.all
   end
+
+  def show
+   @item = Item.find(params[:id])
+  end
+
+  private
+
+   def cart_item_params
+     params.require(:cart_item).permit(:item_id, :amount)
+   end
+
 end
